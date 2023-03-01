017d8b8129170166254cb775e47063ccf05f33ff
bihun <bihun@192.168.0.101>
2023-03-01 15:54:54 +0100

    Main update

--- contactkeeper/src/main/java/application/Main.java
+++ contactkeeper/src/main/java/application/Main.java
@@ -1,7 +1,13 @@
 package application;
 
-public class Main {
-	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
 	/*
 	 * @Override
 	 * 
@@ -9,13 +15,28 @@ public class Main {
 	 * 
 	 */
 	
-	public static void main(String[] args) {
-		
-		/*
-		 * launch(args);
-		 */
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("Login.fxml"));
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
+	public static void main(String[] args) {
+
+		/*
+		 * launch(args);
+		 */
+		
+		launch(args);
+	}
 
 }
